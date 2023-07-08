86e0bdee78739d5a24cb4c3d87cfd97dbdbd99a2
Drea Q <92461958+DreaQuila@users.noreply.github.com>
2023-07-08 03:34:47 +0000

    first commit for excuse

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  let excuse =
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex];
+
+  document.getElementById("excuse").innerHTML = excuse;
+  console.log(excuse);
+  console.log("index " + whoIndex);
+  console.log("index " + actionIndex);
+  console.log("index " + whatIndex);
+  console.log("index " + whenIndex);
 };
